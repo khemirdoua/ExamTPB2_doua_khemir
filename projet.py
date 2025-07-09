08467d9eeb384713f1a3d45a09cfe2e62c848bbc
DOUA <khemirdoua@gmail.com>
2025-07-09 20:11:19 +0100

    Affichage conditionnel si le dossier vide

--- projet.py
+++ projet.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-def produit (T)
-S=0 
-for t in T:
-S*=t
-return S
-=======
-Data[1,3,5]
-Prod=math.Prod (Data)
-print('le produit est :',Prod)
->>>>>>> projetb2
+Data=[1,3,5]
+if Data
+print ('le produit est :', Prod (Data))
+print ('le min est :', min (Data))
+print ('le max est:', max (Data))
+else:
+print ('dossier vide')
 
