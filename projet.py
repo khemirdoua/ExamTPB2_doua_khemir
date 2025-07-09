8a6592b4dc424a0b25db7e60bde87baf001fa8d2
DOUA <khemirdoua@gmail.com>
2025-07-09 20:34:41 +0100

    Fusion finale

--- projet.py
+++ projet.py
@@ -1,8 +1,19 @@
 Data=[1,3,5]
+
 if Data
 print ('le produit est :', Prod (Data))
 print ('le min est :', min (Data))
 print ('le max est:', max (Data))
 else:
 print ('dossier vide')
+
+if Data :
+  print ('le produit est :', Prod(Data))
+
+  print ('le min est :', min (Data))
+  
+  print ('le max est :',max (Data))
+else:
+  print ('dossier vide') 
+
 
